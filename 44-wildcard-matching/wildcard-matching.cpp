41274100e2075e0e97cad2cc37f3c63666b567b1
Tilak-Dewangan19 <tilakdewangan2005@gmail.com>
2026-03-12 12:11:16 +0530

    Time: 59 ms (38.37%) | Memory: 15.9 MB (45.91%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        dp[0][0] = true;
+
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = false;
+        }
+        for (int j = 1; j <= m; j++) {
+            bool flag = true;
+            for (int ii = 1; ii <= j; ii++) {
+                if (p[ii - 1] != '*') {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][j] = flag;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+               else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] | dp[i][j - 1];
+                }
+                else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
