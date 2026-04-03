2d42559872761d4e39294c5c97d34571560d191f
Tilak-Dewangan19 <tilakdewangan2005@gmail.com>
2026-04-03 20:58:37 +0530

    Time: 0 ms (100.00%) | Memory: 59.3 MB (91.91%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -6,34 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(root == NULL) return new TreeNode(val);
+        if (root == NULL)
+            return new TreeNode(val);
         TreeNode* cur = root;
-        while(true){
-            if(cur->val <= val){
-                if(cur->right != NULL) {
+        while (true) {
+            if (cur->val <= val) {
+                if (cur->right != NULL) {
                     cur = cur->right;
-                }
-                else{
-                    cur -> right = new TreeNode(val);
+                } else {
+                    cur->right = new TreeNode(val);
                     break;
                 }
 
-            }
-            else{
-                if(cur->left != NULL) cur = cur->left;
-                else{
+            } else {
+                if (cur->left != NULL)
+                    cur = cur->left;
+                else {
                     cur->left = new TreeNode(val);
                     break;
                 }
             }
         }
         return root;
-        
     }
 };
