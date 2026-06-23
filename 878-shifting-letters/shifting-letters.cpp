8d08172c6ef010ab9979a0d61b5d306d08b2a18c
Tilak-Dewangan19 <tilakdewangan2005@gmail.com>
2026-06-23 21:29:19 +0530

    Time: 7 ms (40.07%) | Memory: 71.6 MB (70.75%) - LeetSync

--- 878-shifting-letters/shifting-letters.cpp
+++ 878-shifting-letters/shifting-letters.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string shiftingLetters(string s, vector<int>& shifts) {
         int n = s.size();
-           long long tot = 0;
+           int tot = 0;
 
         for(int i = n-1; i>=0; i--){
             tot = (tot + shifts[i]) % 26;
