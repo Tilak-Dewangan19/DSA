8844ed52c8a50551fb2eb31da3e8d17c361b85f1
Tilak-Dewangan19 <tilakdewangan2005@gmail.com>
2026-07-04 15:52:20 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (53.63%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+            int n = rooms.size();
+            vector<int>vis(n,0);
+            queue<int>q;
+            q.push(0);
+            vis[0] = 1;
+
+            while(!q.empty()){
+                int node = q.front();
+                q.pop();
+                for(auto it : rooms[node]){
+                    if(!vis[it]){
+                        vis[it] = 1;
+                        q.push(it);
+                    }
+                }
+            }
+            for(auto it : vis){
+                if(it == 0) return false;
+            }
+          
+        return true;
+    }
+};
