bac3f2b588c47cced7709417dd119f3f36bb90f1
Tilak-Dewangan19 <tilakdewangan2005@gmail.com>
2026-08-06 19:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (5.46%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+int digitproduct(int x){
+    int prod = 1;
+    while(x > 0){
+        prod = prod * (x%10);
+        x/=10;
+    }
+    return prod;
+}
+    int smallestNumber(int n, int t) {
+       while(true){
+        if(digitproduct(n) % t == 0) return n;
+        n++;
+
+        
+        
+    }
+    }
+};
