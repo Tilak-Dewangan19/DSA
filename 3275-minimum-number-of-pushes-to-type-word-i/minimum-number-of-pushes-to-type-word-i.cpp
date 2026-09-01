b66229b2956aa103ec829cb5ac27ca4ccba71466
Tilak-Dewangan19 <tilakdewangan2005@gmail.com>
2026-09-01 17:20:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (20.93%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int ans = 0;
+        // int ans = 0;
 
-        for (int i = 0; i < word.size(); i++) {
-            ans += (i / 8) + 1;
+        // for (int i = 0; i < word.size(); i++) {
+        //     ans += (i / 8) + 1;
+        // }
+
+        // return ans;
+         vector<int> freq(26, 0);
+        for (auto& c : word) {
+            freq[c - 'a']++;
         }
-
+        sort(freq.begin(), freq.end(), greater<int>());
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0)
+                break;
+            ans += freq[i]*(i/8 + 1);
+        }
         return ans;
     }
 };
